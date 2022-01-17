cf54c9d892416fcc7e8b02747e4a03f01c86c1f9
Joe Perry <78811050+negra-mantequilla@users.noreply.github.com>
2022-01-16 19:36:54 -0600

    Update connection.js

--- SQL/connection.js
+++ SQL/connection.js
@@ -3,10 +3,10 @@
 const {createConnection} = require('mysql')
 
 let connection = createConnection({
-  host: 'database-3.cz4udqblsmpm.us-east-2.rds.amazonaws.com',
-  user: 'admin',
-  password: 'Jp96860463',
-  database: 'ToDo_Table'
+  host: 
+  user: 
+  password: 
+  database: 
 })
 
 module.exports = connection
